a23a5ead65730dec71344decc19116fb373444dd
Anh Nguyen <andy.a.d.nguyen@zoho.com>
2020-10-12 11:16:10 -0700

    Implemented pseudoclassical subclass HoneyMakerBee.js

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,7 +1,25 @@
 var HoneyMakerBee = function() {
 
   Bee.call(this);
+
+  this.age = 10;
+
+  this.job = 'make honey';
+
+  this.honeyPot = 0;
 };
 
-Bee.prototype = Object.create()
+HoneyMakerBee.prototype = Object.create(Bee.prototype);
+
+HoneyMakerBee.prototype.constructor = HoneyMakerBee;
+
+HoneyMakerBee.prototype.makeHoney = function() {
+
+  this.honeyPot++;
+};
+
+HoneyMakerBee.prototype.giveHoney = function() {
+
+  this.honeyPot--;
+};
 
